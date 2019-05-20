1698fa04adc710a0185de35a11a80a802c70cd7b
Theresa Stocking <nottodayjose@MacBook-Pro.attlocal.net>
2019-05-19 19:30:07 -0500

    Finished But Not Running

---
+++ src/Components/CurrencyDisplay.js
@@ -0,0 +1,14 @@
+//If using a separate component to render the display 
+//Would need to be imported into App.js
+
+// import React from 'react'
+
+// const CurrencyDisplay = (props) => (
+// 	<p>
+// 		US Dollar ${props.amount.toFixed(2)} - {props.currency.name}{' '}
+// 		{props.currency.symbol}
+// 		{(props.amount * props.currency.rate).toFixed(2)}
+// 	</p>
+// )
+
+// export default CurrencyDisplay  
